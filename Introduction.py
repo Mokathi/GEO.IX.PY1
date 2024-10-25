2d76705c87810843a611e68f420db1c1a93a576a
Mokathi <mutuandunda769@gmail.com>
2024-10-25 15:15:05 +0300

    second project to upload

---
+++ Introduction.py
@@ -0,0 +1,5 @@
+#Displaying Values Using The Print Statement
+print("George Mutua.")
+print("Back To The Basics Of Programming")
+print(9)
+print(11-3)
---
+++ Variables.py
@@ -0,0 +1,12 @@
+#A Variable Is A Container Used To Store A Value.
+# = is used to assign values to variables.
+
+school = "eMobilis"
+firstName = "George"
+LastName = "Ndunda"
+number = 100
+num1 = 56
+num2 = 305
+num3 = num1 + num2
+print(num3)
+print(school)
